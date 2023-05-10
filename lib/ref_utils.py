a00ccc4993c60f5600bad0a4f414f14257bc67bb
George Kouros <george.kouros.ece@gmail.com>
2023-05-10 14:53:44 +0200

    Add reflection util functions from refnerf

--- lib/ref_utils.py
+++ lib/ref_utils.py
@@ -14,38 +14,48 @@
 
 """Functions for reflection directions and directional encodings."""
 
-# from . import math
-# import jax.numpy as jnp
-import numpy as np
 import torch
+import numpy as np
 
 
 def reflect(viewdirs, normals):
     """Reflect view directions about normals.
+
     The reflection of a vector v about a unit vector n is a vector u such that
     dot(v, n) = dot(u, n), and dot(u, u) = dot(v, v). The solution to these two
     equations is u = 2 dot(n, v) n - v.
+
     Args:
       viewdirs: [..., 3] array of view directions.
       normals: [..., 3] array of normal directions (assumed to be unit vectors).
+
     Returns:
       [..., 3] array of reflection directions.
     """
-    return 2.0 * jnp.sum(
-        normals * viewdirs, axis=-1, keepdims=True) * normals - viewdirs
+    return 2.0 * torch.sum(
+        normals * viewdirs, dim=-1, keepdims=True) * normals - viewdirs
 
 
-def l2_normalize(x, eps=jnp.finfo(jnp.float32).eps):
+def l2_normalize(x, eps=torch.finfo(torch.float32).eps):
     """Normalize x to unit length along last axis."""
-    return x / jnp.sqrt(jnp.maximum(jnp.sum(x**2, axis=-1, keepdims=True), eps))
+    eps = torch.tensor(eps, device=x.device)
+    return x / torch.sqrt(torch.maximum(torch.sum(x**2, dim=-1, keepdims=True), eps))
 
 
 def compute_weighted_mae(weights, normals, normals_gt):
     """Compute weighted mean angular error, assuming normals are unit length."""
-    one_eps = 1 - jnp.finfo(jnp.float32).eps
-    return (weights * jnp.arccos(
-        jnp.clip((normals * normals_gt).sum(-1), -one_eps,
-                 one_eps))).sum() / weights.sum() * 180.0 / jnp.pi
+    one_eps = torch.tensor(1 - torch.finfo(torch.float32).eps,
+                           device=weights.device)
+    return (weights * torch.arccos(
+        torch.clip((normals * normals_gt).sum(-1), -one_eps,
+                   one_eps))).sum() / weights.sum() * 180.0 / torch.pi
+
+
+def compute_weighted_normal_loss(weights, normals, normals_gt):
+    N = len(weights)
+    n1 = torch.abs(normals - normals_gt).sum(axis=-1)
+    n2 = torch.abs(1 - (normals * normals_gt).sum(axis=-1))
+    return (weights * (n1 + n2)).sum() / weights.sum() / N
 
 
 def generalized_binomial_coeff(a, k):
@@ -55,12 +65,15 @@ def generalized_binomial_coeff(a, k):
 
 def assoc_legendre_coeff(l, m, k):
     """Compute associated Legendre polynomial coefficients.
+
     Returns the coefficient of the cos^k(theta)*sin^m(theta) term in the
     (l, m)th associated Legendre polynomial, P_l^m(cos(theta)).
+
     Args:
       l: associated Legendre polynomial degree.
       m: associated Legendre polynomial order.
       k: power of cos(theta).
+
     Returns:
       A float, the coefficient of the term corresponding to the inputs.
     """
@@ -92,17 +105,22 @@ def get_ml_array(deg_view):
 
 def generate_ide_fn(deg_view):
     """Generate integrated directional encoding (IDE) function.
+
     This function returns a function that computes the integrated directional
     encoding from Equations 6-8 of arxiv.org/abs/2112.03907.
+
     Args:
       deg_view: number of spherical harmonics degrees to use.
+
     Returns:
       A function for evaluating integrated directional encoding.
+
     Raises:
       ValueError: if deg_view is larger than 5.
     """
     if deg_view > 5:
-        raise ValueError('Only deg_view of at most 5 is numerically stable.')
+        print('WARNING: Only deg_view of at most 5 is numerically stable.')
+    #   raise ValueError('Only deg_view of at most 5 is numerically stable.')
 
     ml_array = get_ml_array(deg_view)
     l_max = 2**(deg_view - 1)
@@ -110,17 +128,19 @@ def generate_ide_fn(deg_view):
     # Create a matrix corresponding to ml_array holding all coefficients, which,
     # when multiplied (from the right) by the z coordinate Vandermonde matrix,
     # results in the z component of the encoding.
-    mat = np.zeros((l_max + 1, ml_array.shape[1]))
+    mat = torch.zeros((l_max + 1, ml_array.shape[1]))
     for i, (m, l) in enumerate(ml_array.T):
         for k in range(l - m + 1):
             mat[k, i] = sph_harm_coeff(l, m, k)
 
     def integrated_dir_enc_fn(xyz, kappa_inv):
         """Function returning integrated directional encoding (IDE).
+
         Args:
           xyz: [..., 3] array of Cartesian coordinates of directions to evaluate at.
           kappa_inv: [..., 1] reciprocal of the concentration parameter of the von
             Mises-Fisher distribution.
+
         Returns:
           An array with the resulting IDE.
         """
@@ -129,29 +149,33 @@ def generate_ide_fn(deg_view):
         z = xyz[..., 2:3]
 
         # Compute z Vandermonde matrix.
-        vmz = jnp.concatenate([z**i for i in range(mat.shape[0])], axis=-1)
+        vmz = torch.cat([z**i for i in range(mat.shape[0])], dim=-1)
 
         # Compute x+iy Vandermonde matrix.
-        vmxy = jnp.concatenate([(x + 1j * y)**m for m in ml_array[0, :]], axis=-1)
+        vmxy = torch.cat(
+            [(x + 1j * y)**m for m in ml_array[0, :]], dim=-1)
 
         # Get spherical harmonics.
-        sph_harms = vmxy * math.matmul(vmz, mat)
+        sph_harms = vmxy * torch.matmul(vmz, mat)
 
         # Apply attenuation function using the von Mises-Fisher distribution
         # concentration parameter, kappa.
-        sigma = 0.5 * ml_array[1, :] * (ml_array[1, :] + 1)
-        ide = sph_harms * jnp.exp(-sigma * kappa_inv)
+        sigma = torch.tensor(
+            0.5 * ml_array[1, :] * (ml_array[1, :] + 1), dtype=torch.float32)
+        ide = sph_harms * torch.exp(-sigma * kappa_inv)
 
         # Split into real and imaginary parts and return
-        return jnp.concatenate([jnp.real(ide), jnp.imag(ide)], axis=-1)
+        return torch.cat([torch.real(ide), torch.imag(ide)], dim=-1)
 
     return integrated_dir_enc_fn
 
 
 def generate_dir_enc_fn(deg_view):
     """Generate directional encoding (DE) function.
+
     Args:
       deg_view: number of spherical harmonics degrees to use.
+
     Returns:
       A function for evaluating directional encoding.
     """
